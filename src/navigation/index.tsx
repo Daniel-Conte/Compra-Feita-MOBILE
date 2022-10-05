31718364503e66230c4469227dbb45e3661a04ba
Daniel Conte <182736@upf.br>
2022-10-05 15:01:05 -0300

    chore: :wrench: configura ts paths
    altera importacoes

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -15,16 +15,16 @@ import * as React from "react";
 import { ColorSchemeName, Pressable } from "react-native";
 
 import Colors from "../constants/Colors";
-import useColorScheme from "../hooks/useColorScheme";
-import ModalScreen from "../screens/ModalScreen";
-import NotFoundScreen from "../screens/NotFoundScreen";
-import TabOneScreen from "../screens/TabOneScreen";
-import TabTwoScreen from "../screens/TabTwoScreen";
+import useColorScheme from "@hooks/useColorScheme";
+import ModalScreen from "@screens/ModalScreen";
+import NotFoundScreen from "@screens/NotFoundScreen";
+import TabOneScreen from "@screens/TabOneScreen";
+import TabTwoScreen from "@screens/TabTwoScreen";
 import {
   RootStackParamList,
   RootTabParamList,
   RootTabScreenProps,
-} from "../../types";
+} from "@typings/navigation";
 import LinkingConfiguration from "./LinkingConfiguration";
 
 export default function Navigation({
