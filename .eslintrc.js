f71d5158e963a94de335aeb745e19cb61923b2a8
Daniel Conte <182736@upf.br>
2022-10-05 15:25:15 -0300

    chore: :wrench: configura eslint e prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'prettier'],
+  rules: {},
+};
