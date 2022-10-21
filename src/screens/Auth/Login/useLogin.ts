c624f7dcb1d0e868eaa0c7c636c6e60542f8e64e
Daniel Conte <182736@upf.br>
2022-10-21 10:02:15 -0300

    feat(login): :sparkles: implementa loading e snackbar no login

--- src/screens/Auth/Login/useLogin.ts
+++ src/screens/Auth/Login/useLogin.ts
@@ -2,11 +2,17 @@ import * as SecureStore from 'expo-secure-store';
 
 import authApi from '@services/auth';
 import { AUTH_TOKEN_KEY } from '@config/Constants';
+import useAppStore from '@store/App';
+import parseError from '@utils/parseError';
 import type { LoginFormValues } from './types';
 
 const useLogin = (goBack: () => void) => {
+  const toggleLoading = useAppStore(state => state.toggleLoading);
+  const toggleSnackbar = useAppStore(state => state.toggleSnackbar);
+
   const onSubmit = async (data: LoginFormValues) => {
     try {
+      toggleLoading(true);
       const resp = await authApi.login(data);
 
       if (!resp?.data) throw new Error('Ocorreu um erro no login');
@@ -15,7 +21,11 @@ const useLogin = (goBack: () => void) => {
 
       goBack();
     } catch (error) {
-      console.log(error);
+      const message = parseError(error);
+
+      toggleSnackbar({ title: message, variant: 'danger' });
+    } finally {
+      toggleLoading(false);
     }
   };
 
