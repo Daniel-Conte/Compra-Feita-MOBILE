ab294fc4b91742f926a68e7aecd7ed9b998d05cc
Daniel Conte <182736@upf.br>
2022-10-21 10:29:57 -0300

    feat(login): :sparkles: salva usuario na store ao fazer login

--- src/screens/Auth/Login/useLogin.ts
+++ src/screens/Auth/Login/useLogin.ts
@@ -1,14 +1,18 @@
 import * as SecureStore from 'expo-secure-store';
+import jwtDecode from 'jwt-decode';
 
 import authApi from '@services/auth';
 import { AUTH_TOKEN_KEY } from '@config/Constants';
 import useAppStore from '@store/App';
+import useUserStore from '@store/User';
+import type { User } from '@store/User/types';
 import parseError from '@utils/parseError';
 import type { LoginFormValues } from './types';
 
 const useLogin = (goBack: () => void) => {
   const toggleLoading = useAppStore(state => state.toggleLoading);
   const toggleSnackbar = useAppStore(state => state.toggleSnackbar);
+  const setUser = useUserStore(state => state.setUser);
 
   const onSubmit = async (data: LoginFormValues) => {
     try {
@@ -17,6 +21,8 @@ const useLogin = (goBack: () => void) => {
 
       if (!resp?.data) throw new Error('Ocorreu um erro no login');
 
+      const user = jwtDecode<User>(resp.data);
+      setUser(user);
       await SecureStore.setItemAsync(AUTH_TOKEN_KEY, resp.data);
 
       goBack();
