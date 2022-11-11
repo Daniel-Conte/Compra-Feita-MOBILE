f4caf36b9a65a35f25ca989680ddbab9734e2a2b
Daniel Conte <182736@upf.br>
2022-11-10 22:58:17 -0300

    feat(constantes): :sparkles: adiciona constantes de status pedido e metodo pagamento

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export const STATUS_PEDIDO = {
+  0: 'Aguardando',
+  1: 'Confirmado',
+  2: 'Negado',
+  3: 'Cancelado',
+  4: 'Em preparação',
+  5: 'Finalizado',
+};
+
+export const METODO_PAGAMENTO = {
+  1: 'Cartão',
+  2: 'Dinheiro',
+};
